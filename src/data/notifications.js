21787e03209d08cd9d614e5dbeba7b2413b4e04b
luan4g <luan.ferreira.mads@gmail.com>
2020-05-26 20:13:41 -0300

    Página notificações atualizadas

---
+++ src/data/notifications.js
@@ -0,0 +1,16 @@
+export const dataNot = [
+  {
+    notId: "0",
+    notIcon: require("../../assets/userIcon.png"),
+    notName: "CAEE",
+    notDesc:
+      "Atualização do calendário acadêmico acaba de ser lançado na aba de horário academico para que todos possam acessar e verificar tudo que está pré programado para ocorrer neste ano de 2020, verifiquem assim que possível, para uma possível dúvida.",
+  },
+  {
+    notId: "1",
+    notIcon: require("../../assets/icon.png"),
+    notName: "CRAD",
+    notDesc:
+      "Atualização sdo calendário acadêmico acaba de ser lançado na aba de horário academico para que todos possam acessar e verificar tudo que está pré programado para ocorrer neste ano de 2020, verifiquem assim que possível, para uma possível dúvida.",
+  },
+];
